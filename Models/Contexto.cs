7550d2f78fcc7486955b3e2d3963483c36d34c29
Almir Rogério Camolesi <camolesi@fema.edu.br>
2024-03-20 20:59:02 -0300

    atualiza projeto

--- Models/Contexto.cs
+++ Models/Contexto.cs
@@ -5,10 +5,11 @@ namespace CRUDTATEST2024.Models
     public class Contexto:DbContext
     {
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
-
+       
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Venda> Vendas { get; set; }
         public DbSet<Item> Itens { get; set; }
+
     }
 }
